7d039982c37a579873234e6135921d636c121a5f
KimRass <rmx1000@yonsei.ac.kr>
2023-03-09 19:03:59 +0900

    Transformer pytorch

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -3,18 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# One thing we should be wary of here is that the scope of self attention is limited to the words that occur before a given word. This prevents any information leaks during the training of the model. This is done by masking the words that occur after it for each step. So for step 1, only the first word of the output sequence is NOT masked, for step 2, the first two words are NOT masked and so on.
+batch_size = 4
+vocab_size = 1_000
+seq_len = 380
+d_model=512
 
-d_word_vec=512,
-d_inner=2048,
-n_layers=6,
-d_model=512,
-d_k=64,
-d_v=64,
-dropout=0.1,
-n_position=200,
-trg_emb_prj_weight_sharing=True,
-emb_src_trg_weight_sharing=True
+
+
+class ResidualConnection(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+
+        self.fn = fn
+
+    def forward(self, x):
+        return x + self.fn(x)
 
 
 class ScaledDotProductAttention(nn.Module):
@@ -23,136 +26,106 @@ class ScaledDotProductAttention(nn.Module):
         self.temperature = temperature
         self.dropout = nn.Dropout(attn_dropout)
 
-    def forward(self, q, k, v, mask=None):
-        # attn_scores = torch.matmul(q, k.transpose(dim0=2, dim1=3)) / self.temperature
-        attn_scores = torch.matmul(q, k.transpose(dim0=0, dim1=1)) / self.temperature
+    # def forward(self, q, k, v, mask=None):
+    def forward(self, Q, K, V, mask=None):
+        # attn_scores = torch.matmul(q, k.transpose(dim0=0, dim1=1)) / self.temperature
+        attn_scores = torch.matmul(Q, K.transpose(1, 2)) / self.temperature
 
-        if mask is not None:
-            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+        # if mask is not None:
+        #     attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
 
+        # attn_weights = F.softmax(attn_scores, dim=-1)
+        # dropout = self.dropout(attn_weights)
+        # x = torch.matmul(dropout, v)
         attn_weights = F.softmax(attn_scores, dim=-1)
-        dropout = self.dropout(attn_weights)
-        output = torch.matmul(dropout, v)
-        return output, dropout
+        x = torch.matmul(attn_weights, K)
+        # return x, dropout
+        return x
 
-
-d_model=512
-h=8
-d_k=d_model // h
-d_v=d_model // h
-scaled_dot_product_attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
-q = torch.rand((1, d_model))
-k = torch.rand((1, d_model))
-v = torch.rand((1, d_model))
-scaled_dot_product_attention(q=q, k=k, v=v)
-
-w_qs = nn.Linear(d_model, h * d_k, bias=False)
-w_qs(q).shape
-
-W_V = nn.Linear(d_model, d_v, bias=False)
-W_Q(q), W_K(k), W_V(v)
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, h, d_model, d_k, d_v, dropout=0.1):
+    def __init__(self, n_heads, d_model, dropout=0.1):
         super().__init__()
-
-        self.h = h
-        self.d_k = d_k
-        self.d_v = d_v
-
-        self.w_qs = nn.Linear(d_model, h * d_k, bias=False)
-        self.w_ks = nn.Linear(d_model, h * d_k, bias=False)
-        self.w_vs = nn.Linear(d_model, h * d_v, bias=False)
-        self.fc = nn.Linear(h * d_v, d_model, bias=False)
+        n_heads = 8 # `h`
+        d_k = d_v = d_model // n_heads
         
+        self.n_heads = n_heads
+        self.d_k = self.d_v = d_model // n_heads
+
         self.W_Q = nn.Linear(d_model, d_k, bias=False)
         self.W_K = nn.Linear(d_model, d_k, bias=False)
         self.W_V = nn.Linear(d_model, d_v, bias=False)
-        self.W_O = nn.Linear(h * d_v, d_model, bias=False)
+        self.W_O = nn.Linear(n_heads * d_v, d_model, bias=False)
 
-        self.head = ScaledDotProductAttention(temperature=d_k ** 0.5)
+        self.scaled_dot_product_attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
 
-        self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
-
-    # `Q`: `(batch_size, len_Q, d_model)`
-    # `K`: `(batch_size, len_K, d_model)`
-    # `V`: `(batch_size, len_V, d_model)`
     def forward(self, Q, K, V, mask=None):
-
-        # d_k, d_v, h = self.d_k, self.d_v, self.h
-        batch_size, len_Q, len_K, len_V = Q.size(0), Q.size(1), K.size(1), V.size(1)
-
-        residual = Q
-
-        # Pass through the pre-attention projection: b x lq x (n*dv)
-        # Separate different heads: b x lq x n x dv
-        # q = self.w_qs(q).view(batch_size, len_Q, h, d_k)
-        # k = self.w_ks(k).view(batch_size, len_K, h, d_k)
-        # v = self.w_vs(v).view(batch_size, len_V, h, d_v)
-
-        # Transpose for attention dot product: b x n x lq x dv
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-
-        if mask is not None:
-            mask = mask.unsqueeze(1)   # For head axis broadcasting.
-
-        q, attn = self.head(q, k, v, mask=mask)
-
-        # Transpose to move the head dimension back: b x lq x n x dv
-        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
-        q = q.transpose(1, 2).contiguous().view(batch_size, len_Q, -1)
-        q = self.dropout(self.fc(q))
-        q += residual
-
-        q = self.layer_norm(q)
-        return q, attn
+        heads = [
+            scaled_dot_product_attention(Q=self.W_Q(Q), K=self.W_K(K), V=self.W_V(V))
+            for _ in range(self.n_heads)
+        ]
+        x = torch.cat(heads, axis=2)
+        x = self.W_O(x)
+        return x
 
 
-# class MultiHeadAttention(nn.Module):
-#     def __init__(self, h, d_model, d_k, d_v, dropout=0.1):
-#         super().__init__()
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=2048):
+        super().__init__()
 
-#         self.h = h
-#         self.d_k = d_k
-#         self.d_v = d_v
+        self.w1 = nn.Linear(d_model, d_ff)
+        self.relu = nn.ReLU()
+        self.w2 = nn.Linear(d_ff, d_model)
 
-#         self.w_qs = nn.Linear(d_model, h * d_k, bias=False)
-#         self.w_ks = nn.Linear(d_model, h * d_k, bias=False)
-#         self.w_vs = nn.Linear(d_model, h * d_v, bias=False)
-#         self.fc = nn.Linear(h * d_v, d_model, bias=False)
+    def forward(self, x):
+        x = self.w1(x)
+        x = self.relu(x)
+        x = self.w2(x)
+        return x
 
-#         self.head = ScaledDotProductAttention(temperature=d_k ** 0.5)
 
-#         self.dropout = nn.Dropout(dropout)
-#         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+class Encoder(nn.Module):
+    # `n_layers`: $N$
+    def __init__(self, n_heads, d_model, n_layers=6):
+        super().__init__()
 
-#     def forward(self, q, k, v, mask=None):
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.n_layers = n_layers
 
-#         d_k, d_v, h = self.d_k, self.d_v, self.h
-#         batch_size, len_Q, len_K, len_V = q.size(0), q.size(1), k.size(1), v.size(1)
+        self.multi_head_attn = MultiHeadAttention(n_heads=n_heads, d_model=d_model)
+        # residual_connection = ResidualConnection(fn=multi_head_attn)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.feed_forward = PositionwiseFeedForward(d_model=d_model)
+        self.norm2 = nn.LayerNorm(d_model)
 
-#         residual = q
+    def forward(self, x):
+        for _ in range(n_layers):
+            # multi_head_attn(Q=x, K=x, V=x).shape
+            x += self.multi_head_attn(Q=x, K=x, V=x)
+            x = self.norm1(x)
+            x += self.feed_forward(x)
+            x = self.norm2(x)
+        return x
 
-#         # Pass through the pre-attention projection: b x lq x (n*dv)
-#         # Separate different heads: b x lq x n x dv
-#         q = self.w_qs(q).view(batch_size, len_Q, h, d_k)
-#         k = self.w_ks(k).view(batch_size, len_K, h, d_k)
-#         v = self.w_vs(v).view(batch_size, len_V, h, d_v)
 
-#         # Transpose for attention dot product: b x n x lq x dv
-#         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+input = torch.randint(low=0, high=vocab_size, size=(batch_size, seq_len))
+embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+x = embedding(input)
+x.shape
+# positional_encoding = nn.PositionalEncoding()
+# x = positional_encoding(x)
 
-#         if mask is not None:
-#             mask = mask.unsqueeze(1)   # For head axis broadcasting.
 
-#         q, attn = self.head(q, k, v, mask=mask)
 
-#         # Transpose to move the head dimension back: b x lq x n x dv
-#         # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
-#         q = q.transpose(1, 2).contiguous().view(batch_size, len_Q, -1)
-#         q = self.dropout(self.fc(q))
-#         q += residual
+d_word_vec=512
+d_inner=2048
+n_layers=6
+d_k=64
+d_v=64
+dropout=0.1
+n_position=200
+trg_emb_prj_weight_sharing=True
+emb_src_trg_weight_sharing=True
 
-#         q = self.layer_norm(q)
-#         return q, attn
+
