4962fab15643198dd51aa665a22d974560dd7e74
KimRass <rmx1000@yonsei.ac.kr>
2023-05-22 18:58:52 +0900

    Working on; BERT vocab and MLM

--- bert/data.py
+++ bert/data.py
@@ -3,137 +3,66 @@
 
 import torch
 from torch.utils.data import Dataset
-import tqdm
+from tqdm import tqdm
 import random
+import json
+from pathlib import Path
+# import pickle as pk
+
+from wordpiece import collect_corpus, tokenize
 
 
 class BERTDataset(Dataset):
-    def __init__(self, corpus_path, vocab, seq_len, encoding="utf-8", corpus_lines=None, on_memory=True):
-        self.vocab = vocab
-        self.seq_len = seq_len
+    def __init__(self, vocab_path, corpus_dir):
+        # super().__init__()
 
-        self.on_memory = on_memory
-        self.corpus_lines = corpus_lines
-        self.corpus_path = corpus_path
-        self.encoding = encoding
+        self.vocab_path = vocab_path
+        self.corpus_dir = corpus_dir
 
-        with open(corpus_path, "r", encoding=encoding) as f:
-            if self.corpus_lines is None and not on_memory:
-                for _ in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines):
-                    self.corpus_lines += 1
+        with open(vocab_path, mode="r") as f:
+            self.vocab = json.load(f)
+        self.vocab_size = len(self.vocab)
+        self.pad_id = self.vocab["[PAD]"]
+        self.unk_id = self.vocab["[UNK]"]
+        self.cls_id = self.vocab["[CLS]"]
+        self.sep_id = self.vocab["[SEP]"]
+        self.mask_id = self.vocab["[MASK]"]
 
-            if on_memory:
-                self.lines = [line[:-1].split("\t")
-                              for line in tqdm.tqdm(f, desc="Loading Dataset", total=corpus_lines)]
-                self.corpus_lines = len(self.lines)
+        self.id_to_token = {v:k for k, v in self.vocab.items()}
 
-        if not on_memory:
-            self.file = open(corpus_path, "r", encoding=encoding)
-            self.random_file = open(corpus_path, "r", encoding=encoding)
-
-            for _ in range(random.randint(self.corpus_lines if self.corpus_lines < 1000 else 1000)):
-                self.random_file.__next__()
-
-    def __len__(self):
-        return self.corpus_lines
-
-    def __getitem__(self, item):
-        t1, t2, is_next_label = self.random_sent(item)
-        t1_random, t1_label = self.random_word(t1)
-        t2_random, t2_label = self.random_word(t2)
-
-        # [CLS] tag = SOS tag, [SEP] tag = EOS tag
-        t1 = [self.vocab.sos_index] + t1_random + [self.vocab.eos_index]
-        t2 = t2_random + [self.vocab.eos_index]
-
-        t1_label = [self.vocab.pad_index] + t1_label + [self.vocab.pad_index]
-        t2_label = t2_label + [self.vocab.pad_index]
-
-        segment_label = ([1 for _ in range(len(t1))] + [2 for _ in range(len(t2))])[:self.seq_len]
-        bert_input = (t1 + t2)[:self.seq_len]
-        bert_label = (t1_label + t2_label)[:self.seq_len]
-
-        padding = [self.vocab.pad_index for _ in range(self.seq_len - len(bert_input))]
-        bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
-
-        output = {"bert_input": bert_input,
-                  "bert_label": bert_label,
-                  "segment_label": segment_label,
-                  "is_next": is_next_label}
-
-        return {key: torch.tensor(value) for key, value in output.items()}
-
-    def random_word(self, sentence):
-        tokens = sentence.split()
-        output_label = []
-
-        for i, token in enumerate(tokens):
-            prob = random.random()
-            if prob < 0.15:
-                prob /= 0.15
-
-                # 80% randomly change token to mask token
-                if prob < 0.8:
-                    tokens[i] = self.vocab.mask_index
-
-                # 10% randomly change token to random token
-                elif prob < 0.9:
-                    tokens[i] = random.randrange(len(self.vocab))
-
-                # 10% randomly change token to current token
-                else:
-                    tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
-
-                output_label.append(self.vocab.stoi.get(token, self.vocab.unk_index))
-
-            else:
-                tokens[i] = self.vocab.stoi.get(token, self.vocab.unk_index)
-                output_label.append(0)
-
-        return tokens, output_label
-
-    def random_sent(self, index):
-        t1, t2 = self.get_corpus_line(index)
-
-        # output_text, label(isNotNext:0, isNext:1)
-        if random.random() > 0.5:
-            return t1, t2, 1
-        else:
-            return t1, self.get_random_line(), 0
-
-    def get_corpus_line(self, item):
-        if self.on_memory:
-            return self.lines[item][0], self.lines[item][1]
-        else:
-            line = self.file.__next__()
-            if line is None:
-                self.file.close()
-                self.file = open(self.corpus_path, "r", encoding=self.encoding)
-                line = self.file.__next__()
-
-            t1, t2 = line[:-1].split("\t")
-            return t1, t2
-
-    def get_random_line(self):
-        if self.on_memory:
-            return self.lines[random.randrange(len(self.lines))][1]
-
-        line = self.file.__next__()
-        if line is None:
-            self.file.close()
-            self.file = open(self.corpus_path, "r", encoding=self.encoding)
-            for _ in range(random.randint(self.corpus_lines if self.corpus_lines < 1000 else 1000)):
-                self.random_file.__next__()
-            line = self.random_file.__next__()
-        return line[:-1].split("\t")[1]
-
-
-ds = BERTDataset(corpus_path="/Users/jongbeomkim/Downloads/books1/epubtxt/the-greylands-volume-i.epub.txt", v)
-
-
-class CustomDataset(Dataset):
-    def __init__(self):
+        self.corpus = collect_corpus(corpus_dir=corpus_dir, add_empty_string=True)
+      
         
     def __len__(self):
+        return len(self.corpus)
     
     def __getitem__(self, id_):
+        sent = self.corpus[id_]
+        tokens = tokenize(text=sent, vocab=self.vocab)
+
+        ids, label = [self.cls_id], [self.cls_id]
+        label_sent = ""
+        for token in tokens:
+            id_ = self.vocab.get(token, self.unk_id)
+            label.append(id_)
+            if random.random() < 0.15:
+                sampled = random.random()
+                if sampled < 0.8:
+                    id_ = self.mask_id
+                elif sampled < 0.9:
+                    id_ = random.randrange(self.vocab_size)
+            ids.append(id_)
+            label_sent += (" " + self.id_to_token[id_]).replace(" ##", "")
+        ids += [self.sep_id]
+        label += [self.sep_id]
+
+        dic = {"sentence": sent, "ids": ids, "label": label, "label_sentence": label_sent}
+        return dic
+
+vocab_path = "/Users/jongbeomkim/Desktop/workspace/transformer_based_models/bert/vocab.json"
+corpus_dir = "/Users/jongbeomkim/Documents/datasets/bookcorpus_subset"
+ds = BERTDataset(vocab_path=vocab_path, corpus_dir=corpus_dir)
+id_ = 3
+print(ds[id_])
+print(ds[id_]["ids"])
+print(ds[id_]["label"])
